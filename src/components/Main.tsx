d46b9fbb8ad029085ccafabc5b71d1babce1c122
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2020-08-22 21:32:52 +0300

    [+] Initial commit v.0.1.0

---
+++ src/components/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.scss';
+
+function Main() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Main;
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path'),
+    webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: ['./src/index.tsx'],
+        vendor: ['react', 'react-dom']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].bundle.js'
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.svg$/,
+                use: ['svg-url-loader'],
+            },
+
+            {enforce: "pre", test: /\.js$/, loader: "source-map-loader"}
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
